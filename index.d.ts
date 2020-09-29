8a38b90f9e76a96195d81c518c19b5ce8500edab
厚发 <houfa.zxh@antfin.com>
2020-09-29 16:17:29 +0800

    feat: export const

--- index.d.ts
+++ index.d.ts
@@ -10,3 +10,6 @@ export function Context(constructor: Function): any;
 export function Application(constructor: Function): any;
 
 export function getComponent(constructor: any, ctx: any, injectType?: InjectType): any;
+
+export const INJECTED_TAG_CLASS: Symbol;
+export const INJECTED_TAG_PROP: Symbol;
